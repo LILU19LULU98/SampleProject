988258be8901ac7a850576a24634011f4f57f23e
dell <dell@DESKTOP-TA92456>
2023-03-25 15:11:29 +0530

    creating new class of food

---
+++ src/test/java/com/oh/Food.java
@@ -0,0 +1,10 @@
+package com.oh;
+
+public class Food {
+	public void cook() {
+		System.out.println("paper chicken");
+	}
+	public static void main(String[] args) {
+		System.out.println("today eat kuska");
+	}
+}
